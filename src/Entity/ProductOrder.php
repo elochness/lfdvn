f715746d8c56e4b95fe6b58e480864bb05989e40
elochness <pierre.francois@outlook.com>
2020-05-05 11:32:20 +0200

    Adding getter and setter for Product order entity

--- src/Entity/ProductOrder.php
+++ src/Entity/ProductOrder.php
@@ -2,6 +2,9 @@
 
 namespace App\Entity;
 
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,6 +16,7 @@ use Doctrine\ORM\Mapping as ORM;
 class ProductOrder
 {
     /**
+     * Product Order ID
      * @var int
      *
      * @ORM\Column(name="id", type="integer", nullable=false)
@@ -22,13 +26,15 @@ class ProductOrder
     private $id;
 
     /**
-     * @var \DateTime
+     * Delivery date of the product order
+     * @var DateTime
      *
      * @ORM\Column(name="delivery_date", type="date", nullable=false)
      */
     private $deliveryDate;
 
     /**
+     * Comment of the product order
      * @var string|null
      *
      * @ORM\Column(name="comment", type="text", length=65535, nullable=true)
@@ -36,13 +42,15 @@ class ProductOrder
     private $comment;
 
     /**
-     * @var \DateTime
+     * Product order creation date
+     * @var DateTime
      *
      * @ORM\Column(name="created_at", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
      */
     private $createdAt = 'CURRENT_TIMESTAMP';
 
     /**
+     * User of the product order
      * @var \User
      *
      * @ORM\ManyToOne(targetEntity="User")
@@ -53,4 +61,159 @@ class ProductOrder
     private $user;
 
 
+    /**
+     * Items of product order
+     * @var Collection
+     * @ORM\OneToMany(targetEntity="ProductOrderItem", mappedBy="productOrder", cascade={"persist"})
+     */
+    private $items;
+
+    /**
+     * ProductOrder constructor.
+     */
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+    }
+
+    /**
+     * Get Product order ID
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get delivery date of the product order
+     * @return DateTime
+     */
+    public function getDeliveryDate(): DateTime
+    {
+        return $this->deliveryDate;
+    }
+
+    /**
+     * Set delivery date of the product order
+     * @param DateTime $deliveryDate
+     */
+    public function setDeliveryDate(DateTime $deliveryDate): void
+    {
+        $this->deliveryDate = $deliveryDate;
+    }
+
+    /**
+     * Get comment of the product order
+     * @return string|null
+     */
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set comment of the product order
+     * @param string|null $comment
+     */
+    public function setComment(?string $comment): void
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get product order creation date
+     * @return DateTime
+     */
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set product order creation date
+     * @param DateTime $createdAt
+     */
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * Get user of the product order
+     * @return \User
+     */
+    public function getUser(): \User
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set user of the product order
+     * @param \User $user
+     */
+    public function setUser(\User $user): void
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get items of the product order
+     * @return Collection
+     */
+    public function getItems(): Collection
+    {
+        return $this->items;
+    }
+
+    /**
+     * Set user items of the product order
+     * @param Collection $items
+     */
+    public function setItems(Collection $items): void
+    {
+        $this->items = $items;
+    }
+
+    /**
+     * Add an item of the product order
+     * @param ProductOrderItem $item
+     */
+    public function addItem(ProductOrderItem $item): void
+    {
+        $this->items[] = $item;
+    }
+
+    /**
+     * Remove item of the product order
+     * @param ProductOrderItem $item
+     */
+    public function removeItem(ProductOrderItem $item): void
+    {
+        $this->items->removeElement($item);
+    }
+
+    /**
+     * Get information of the product order.
+     * @return string Name of purchase
+     */
+    public function __toString(): string
+    {
+        return $this->id.' - '.$this->createdAt;
+    }
+
+    /**
+     * Get full price of the product order
+     * @return float
+     */
+    public function getFullPrice()
+    {
+        $total = 0.0;
+        foreach ($this->getItems() as $item) {
+            /* @var ProductOrderItem $item */
+            $total += $item->getTotalPrice();
+        }
+
+        return $total;
+    }
 }
