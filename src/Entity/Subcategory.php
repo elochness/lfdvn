02ea2235794d6745471283e3d2636572ae690994
elochness <pierre.francois@outlook.com>
2020-05-05 15:20:03 +0200

    Adding getter and setter for subcategory entity

--- src/Entity/Subcategory.php
+++ src/Entity/Subcategory.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -13,6 +14,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Subcategory
 {
     /**
+     * Subcategory ID
      * @var int
      *
      * @ORM\Column(name="id", type="integer", nullable=false)
@@ -22,6 +24,7 @@ class Subcategory
     private $id;
 
     /**
+     * Name of subcategory
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=50, nullable=false)
@@ -29,14 +32,16 @@ class Subcategory
     private $name;
 
     /**
+     * Indication whether the category is activated
      * @var bool
      *
      * @ORM\Column(name="enabled", type="boolean", nullable=false)
      */
-    private $enabled;
+    private $enabled = true;
 
     /**
-     * @var \Category
+     * Category of the subcategory
+     * @var Category
      *
      * @ORM\ManyToOne(targetEntity="Category")
      * @ORM\JoinColumns({
@@ -45,5 +50,126 @@ class Subcategory
      */
     private $category;
 
+    /**
+     * @var Product[]
+     * @ORM\OneToMany(targetEntity="Product", mappedBy="subcategory")
+     */
+    private $products;
+
+    /**
+     * Subcategory constructor.
+     */
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    /**
+     * Get subcategory ID
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get name of the subcategory
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set name of the subcategory
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get indication whether the category is activated
+     * @return bool
+     */
+    public function isEnabled(): bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * Set indication whether the category is activated
+     * @param bool $enabled
+     */
+    public function setEnabled(bool $enabled): void
+    {
+        $this->enabled = $enabled;
+    }
+
+    /**
+     * Get category of the subcategory
+     * @return Category
+     */
+    public function getCategory(): Category
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set category of the subcategory
+     * @param Category $category
+     */
+    public function setCategory(Category $category): void
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * Get products of the subcategory
+     * @return Product[]
+     */
+    public function getProducts(): array
+    {
+        return $this->products;
+    }
+
+    /**
+     * Set products of the subcategory
+     * @param Product[] $products
+     */
+    public function setProducts(array $products): void
+    {
+        $this->products = $products;
+    }
+    /**
+     * Add product.
+     * @param Product $product
+     * @return Category
+     */
+    public function addProduct(Product $product): void
+    {
+        $this->products[] = $product;
+    }
+
+    /**
+     * Remove product.
+     * @param Product $product
+     */
+    public function removeProduct(Product $product): void
+    {
+        $this->products->removeElement($product);
+    }
+
+    /**
+     * Get information of subcategory.
+     * @return string Name of subcategory
+     */
+    public function __toString(): string
+    {
+        return $this->getName();
+    }
 
 }
