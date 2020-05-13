e0a68b0b77924026c4b1bdc1fe68d86ea5a20b46
elochness <pierre.francois@outlook.com>
2020-05-13 17:45:33 +0200

    Fix contact us template

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
 use App\Entity\Schedule;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Cache;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class CompanyController extends AbstractController
 {
     /**
-     * @Route("/company", name="company_index")
+     * @Route({
+     *     "fr": "/a-propos",
+     *     "en": "/about-us"
+     * }, name="company_index")
+     * @Cache(smaxage="10")
      */
     public function index()
     {
+        $repository = $this->getDoctrine()->getRepository(Company::class);
+        $company = $repository->find(1);
         return $this->render('company/index.html.twig', [
-            'controller_name' => 'CompanyController',
+            'company' => $company,
         ]);
     }
 
@@ -24,7 +32,7 @@ class CompanyController extends AbstractController
         $repository = $this->getDoctrine()->getRepository(Schedule::class);
         $schedule = $repository->find(1);
 
-        return $this->render('enterprise/_schedule.html.twig', [
+        return $this->render('company/_schedule.html.twig', [
             'schedule' => $schedule,
         ]);
     }
